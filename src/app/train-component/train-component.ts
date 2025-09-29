c110d9ba9a83e90c2edabdc804c56659acf55c1d
Timo Neske <neske.timo93@gmail.com>
2025-09-29 15:20:38 +0200

    init train-component

---
+++ src/app/train-component/train-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-train-component',
+  imports: [],
+  templateUrl: './train-component.html',
+  styleUrl: './train-component.css'
+})
+export class TrainComponent {
+
+}
