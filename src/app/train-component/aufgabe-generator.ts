de185388ea350504386fa281b726f06935718514
Timo Neske <neske.timo93@gmail.com>
2025-09-30 11:05:11 +0200

    implement method generateAufgabe and tested it

--- src/app/train-component/aufgabe-generator.ts
+++ src/app/train-component/aufgabe-generator.ts
@@ -1,7 +1,8 @@
-import {ModusTyp} from './modus';
-import {Schwierigkeit} from './schwierigkeit';
-import config from '../../../assets/config/aufgaben-generator.config.json';
+import {Schwierigkeit} from './domain/schwierigkeit';
+import config from '../../assets/config/aufgaben-generator.config.json';
 import {Injectable} from '@angular/core';
+import {Aufgabe} from './domain/aufgabe';
+import {ModusTyp} from './domain/modus';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,6 @@ export class AufgabeGenerator {
   modus : ModusTyp;
   schwierigkeit : Schwierigkeit;
   numberRange : number[] = [];
-  zaehlerRange : number[] = [];
-  nennerRange : number[] = [];
   terms : number = -1;
 
   constructor(modus : ModusTyp,
@@ -20,7 +19,87 @@ export class AufgabeGenerator {
     this.schwierigkeit = schwierigkeit;
     this.setData();
     this.debugPrint();
+  }
 
+  public generateAufgabe() : Aufgabe {
+
+    let terms = this.generateTerms()
+    let term : string = "";
+    let loesung : number = 0;
+
+    switch (this.modus) {
+      case ModusTyp.ADDITION: {
+        loesung = terms.reduce((sum, n) => sum + n, 0);
+        term = terms.join("+");
+        break;
+      }
+      case ModusTyp.SUBTRAKTION: {
+        loesung = terms.slice(1).reduce((diff, n) => diff - n, terms[0]);
+        term = terms.join("-");
+        break;
+      }
+      case ModusTyp.MULTIPLIKATION: {
+        loesung = terms.reduce((prod, n) => prod * n, 1);
+        term = terms.join("*");
+      break;
+      }
+      case ModusTyp.DIVISION: {
+        loesung = terms.slice(1).reduce((quot, n) => quot / n, terms[0]);
+        term = terms.join("/");
+        break;
+      }
+    }
+
+    return {
+      term: term,
+      loesung: loesung,
+    };
+  }
+
+  public generateTerms() : number[] {
+    let terms = []
+    if(this.modus !== ModusTyp.DIVISION) {
+      for(let i = 0; i < this.terms; i++) {
+        const number = this.randomInclusive(this.numberRange[0], this.numberRange[1]);
+        terms.push(number);
+      }
+    } else {
+      // Könnte eine 0 zufällig generiert werden? Das geschieht nur, wenn das Interval eine linke Schranke <= 0 hat
+      if(Math.min(this.numberRange[0], this.numberRange[1]) <= 0) {
+        // Es darf keine 0 generiert werden, versuche Numbergeneration im schlechtesten Fall 10x
+        let number = 0
+        let j = 100
+        for(let i = 0; i < this.terms; i++) {
+          while(number == 0 && j > 0) {
+            number = this.randomInclusive(this.numberRange[0], this.numberRange[1]);
+            j--;
+          }
+          terms.push(number);
+        }
+        // Falls doch im seltenen Fall eine 0 generiert wurde, setze sie auf [1,10]
+        for(let i = 0; i < this.terms; i++) {
+          if(terms[i] == 0) terms[i] = this.randomInclusive(1,10);
+        }
+      } else {
+        // 0 kann nicht generiert werden
+        const number = this.randomInclusive(this.numberRange[0], this.numberRange[1]);
+        terms.push(number);
+      }
+    }
+
+    return terms;
+  }
+
+  public randomInclusive(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  getNumberRange() : number[] {
+    return this.numberRange;
+  }
+
+  getTerms() : number {
+    return this.terms;
   }
 
   private setData() : void {
@@ -88,20 +167,17 @@ export class AufgabeGenerator {
       case ModusTyp.DIVISION: {
         switch (this.schwierigkeit) {
           case Schwierigkeit.EINFACH: {
-            this.zaehlerRange = config.div["0"].zaehlerRange;
-            this.nennerRange = config.div["0"].nennerRange;
+            this.numberRange = config.div["0"].numberRange;
             this.terms = config.div["0"].terms;
             break
           }
           case Schwierigkeit.MITTEL: {
-            this.zaehlerRange = config.div["1"].zaehlerRange;
-            this.nennerRange = config.div["1"].nennerRange;
+            this.numberRange = config.div["1"].numberRange;
             this.terms = config.div["1"].terms;
             break;
           }
           case Schwierigkeit.SCHWER: {
-            this.zaehlerRange = config.div["2"].zaehlerRange;
-            this.nennerRange = config.div["2"].nennerRange;
+            this.numberRange = config.div["2"].numberRange;
             this.terms = config.div["2"].terms;
             break;
           }
@@ -114,8 +190,6 @@ export class AufgabeGenerator {
   private debugPrint() : void {
     console.log("init generator with config:")
     console.log(this.numberRange)
-    console.log(this.zaehlerRange)
-    console.log(this.nennerRange)
     console.log(this.terms)
   }
 
--- src/app/train-component/aufgaben-generator.spec.ts
+++ src/app/train-component/aufgaben-generator.spec.ts
@@ -5,13 +5,133 @@ import {Schwierigkeit} from './domain/schwierigkeit';
 describe('AufgabenGenerator', () => {
   let aufgabenGenerator : AufgabeGenerator;
 
-  it('wird mit einem ModusTyp und Schwierigkeit korrekt mit den jeweiligen Konfigurationdaten befüllt', () => {
+  it('wird mit einem ModusTyp.ADDITION und Schwierigkeit korrekt mit den jeweiligen Konfigurationdaten befüllt', () => {
     aufgabenGenerator = new AufgabeGenerator(ModusTyp.ADDITION, Schwierigkeit.EINFACH)
 
     const numberRange = aufgabenGenerator.getNumberRange();
     const terms = aufgabenGenerator.getTerms();
 
-    expect(numberRange.length).toBeGreaterThan(0);
+    expect(numberRange.length).toEqual(2)
     expect(terms).toBeGreaterThan(-1)
+  });
+
+  it('generiert für ModusTyp.Addition und Schwierigkeit.EINFACH ein korrektes Aufgaben-Objekt', () => {
+    aufgabenGenerator = new AufgabeGenerator(ModusTyp.ADDITION, Schwierigkeit.EINFACH)
+    spyOn(aufgabenGenerator as any, 'generateTerms').and.returnValues([22,11]);
+
+    const aufgabe = aufgabenGenerator.generateAufgabe()
+
+    expect(aufgabe.term).toEqual("22+11")
+    expect(aufgabe.loesung).toEqual(33);
+  });
+
+  it('generiert für ModusTyp.Addition und Schwierigkeit.MITTEL ein korrektes Aufgaben-Objekt', () => {
+    aufgabenGenerator = new AufgabeGenerator(ModusTyp.ADDITION, Schwierigkeit.MITTEL)
+    spyOn(aufgabenGenerator as any, 'generateTerms').and.returnValues([10000,10, 66]);
+
+    const aufgabe = aufgabenGenerator.generateAufgabe()
+
+    expect(aufgabe.term).toEqual("10000+10+66")
+    expect(aufgabe.loesung).toEqual(10076);
+  });
+
+  it('generiert für ModusTyp.Addition und Schwierigkeit.SCHWER ein korrektes Aufgaben-Objekt', () => {
+    aufgabenGenerator = new AufgabeGenerator(ModusTyp.ADDITION, Schwierigkeit.SCHWER)
+    spyOn(aufgabenGenerator as any, 'generateTerms').and.returnValues([10000,99999,10, 22]);
+
+    const aufgabe = aufgabenGenerator.generateAufgabe()
+
+    expect(aufgabe.term).toEqual("10000+99999+10+22")
+    expect(aufgabe.loesung).toEqual(110031);
+  });
+
+  it('generiert für ModusTyp.SUBTRAKTION und Schwierigkeit.EIFNACH ein korrektes Aufgaben-Objekt', () => {
+    aufgabenGenerator = new AufgabeGenerator(ModusTyp.SUBTRAKTION, Schwierigkeit.EINFACH)
+    spyOn(aufgabenGenerator as any, 'generateTerms').and.returnValues([-20,15]);
+
+    const aufgabe = aufgabenGenerator.generateAufgabe()
+
+    expect(aufgabe.term).toEqual("-20-15")
+    expect(aufgabe.loesung).toEqual(-35);
+  });
+
+  it('generiert für ModusTyp.SUBTRAKTION und Schwierigkeit.MITTEL ein korrektes Aufgaben-Objekt', () => {
+    aufgabenGenerator = new AufgabeGenerator(ModusTyp.SUBTRAKTION, Schwierigkeit.MITTEL)
+    spyOn(aufgabenGenerator as any, 'generateTerms').and.returnValues([-200,-500,85]);
+
+    const aufgabe = aufgabenGenerator.generateAufgabe()
+
+    expect(aufgabe.term).toEqual("-200--500-85")
+    expect(aufgabe.loesung).toEqual(215);
+  });
+
+  it('generiert für ModusTyp.SUBTRAKTION und Schwierigkeit.SCHWER ein korrektes Aufgaben-Objekt', () => {
+    aufgabenGenerator = new AufgabeGenerator(ModusTyp.SUBTRAKTION, Schwierigkeit.SCHWER)
+    spyOn(aufgabenGenerator as any, 'generateTerms').and.returnValues([-200,-500,85,215]);
+
+    const aufgabe = aufgabenGenerator.generateAufgabe()
+
+    expect(aufgabe.term).toEqual("-200--500-85-215")
+    expect(aufgabe.loesung).toEqual(0);
+  });
+
+  it('generiert für ModusTyp.MULTPILKATION und Schwierigkeit.EINFACH ein korrektes Aufgaben-Objekt', () => {
+    aufgabenGenerator = new AufgabeGenerator(ModusTyp.MULTIPLIKATION, Schwierigkeit.EINFACH)
+    spyOn(aufgabenGenerator as any, 'generateTerms').and.returnValues([-3,5]);
+
+    const aufgabe = aufgabenGenerator.generateAufgabe()
+
+    expect(aufgabe.term).toEqual("-3*5")
+    expect(aufgabe.loesung).toEqual(-15);
+  });
+
+  it('generiert für ModusTyp.MULTPILKATION und Schwierigkeit.MITTEL ein korrektes Aufgaben-Objekt', () => {
+    aufgabenGenerator = new AufgabeGenerator(ModusTyp.MULTIPLIKATION, Schwierigkeit.MITTEL)
+    spyOn(aufgabenGenerator as any, 'generateTerms').and.returnValues([-15,11]);
+
+    const aufgabe = aufgabenGenerator.generateAufgabe()
+
+    expect(aufgabe.term).toEqual("-15*11")
+    expect(aufgabe.loesung).toEqual(-165);
+  });
+
+  it('generiert für ModusTyp.MULTPILKATION und Schwierigkeit.SCHWER ein korrektes Aufgaben-Objekt', () => {
+    aufgabenGenerator = new AufgabeGenerator(ModusTyp.MULTIPLIKATION, Schwierigkeit.SCHWER)
+    spyOn(aufgabenGenerator as any, 'generateTerms').and.returnValues([-20,11,-2]);
+
+    const aufgabe = aufgabenGenerator.generateAufgabe()
+
+    expect(aufgabe.term).toEqual("-20*11*-2")
+    expect(aufgabe.loesung).toEqual(440);
+  });
+
+  it('generiert für ModusTyp.DIVISION und Schwierigkeit.EINFACH ein korrektes Aufgaben-Objekt', () => {
+    aufgabenGenerator = new AufgabeGenerator(ModusTyp.DIVISION, Schwierigkeit.EINFACH)
+    spyOn(aufgabenGenerator as any, 'generateTerms').and.returnValue([90,-10])
+
+    const aufgabe = aufgabenGenerator.generateAufgabe()
+
+    expect(aufgabe.term).toEqual("90/-10")
+    expect(aufgabe.loesung).toEqual(-9);
+  });
+
+  it('generiert für ModusTyp.DIVISION und Schwierigkeit.MITTEL ein korrektes Aufgaben-Objekt', () => {
+    aufgabenGenerator = new AufgabeGenerator(ModusTyp.DIVISION, Schwierigkeit.EINFACH)
+    spyOn(aufgabenGenerator as any, 'generateTerms').and.returnValue([220,4])
+
+    const aufgabe = aufgabenGenerator.generateAufgabe()
+
+    expect(aufgabe.term).toEqual("220/4")
+    expect(aufgabe.loesung).toEqual(55);
+  });
+
+  it('generiert für ModusTyp.DIVISION und Schwierigkeit.SCHWER ein korrektes Aufgaben-Objekt', () => {
+    aufgabenGenerator = new AufgabeGenerator(ModusTyp.DIVISION, Schwierigkeit.EINFACH)
+    spyOn(aufgabenGenerator as any, 'generateTerms').and.returnValue([500,5,-2])
+
+    const aufgabe = aufgabenGenerator.generateAufgabe()
+
+    expect(aufgabe.term).toEqual("500/5/-2")
+    expect(aufgabe.loesung).toEqual(-50);
   });
 });
