2daeb55443e05d3040fb488b2ae041c0b87b72ef
Timo Neske <neske.timo93@gmail.com>
2025-09-30 18:58:10 +0200

    make class injectable

--- src/app/api/uebung-api.service.ts
+++ src/app/api/uebung-api.service.ts
@@ -1,11 +1,16 @@
 import {HttpClient} from '@angular/common/http';
-import {inject} from '@angular/core';
 import {UebungEvent} from '../train-component/domain/uebung';
+import {inject, Injectable} from '@angular/core';
 
+@Injectable({
+  providedIn: 'root'
+})
 export class UebungApiService {
   httpClient : HttpClient = inject(HttpClient);
 
+  BASE_URL = "http://localhost:11000"
+
   postUebungEvent(event : UebungEvent) : void {
-    this.httpClient.post<any>("ENDPOINT", event).subscribe({})
+    this.httpClient.post<any>(this.BASE_URL+ "/uebung-event", event).subscribe({})
   }
 }
