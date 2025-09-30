a17d85c22fabeb214bcd5ae331d0e11684ff13e6
Timo Neske <neske.timo93@gmail.com>
2025-10-01 00:05:08 +0200

    update component

--- src/app/history-component/history-component.ts
+++ src/app/history-component/history-component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {UebungApiService} from '../api/uebung-api.service';
+import {UebungEvent} from '../train-component/domain/uebung';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-history-component',
-  imports: [],
+  imports: [NgForOf],
   templateUrl: './history-component.html',
   styleUrls: ['./history-component.css', '../debug.css']
 })
-export class HistoryComponent {
+export class HistoryComponent implements OnInit {
+  uebungApiService = inject(UebungApiService)
+  uebungEvents : UebungEvent[] = [];
 
+  ngOnInit(): void {
+    this.uebungApiService.getAllEvents("testuser").subscribe((events : UebungEvent[]) => {
+      console.log(events);
+      this.uebungEvents = events;
+    })
+  }
 }
