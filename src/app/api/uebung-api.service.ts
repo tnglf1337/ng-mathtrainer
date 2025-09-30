5fa51949ffed4d485c300cafa6984bdcdc0cee4e
Timo Neske <neske.timo93@gmail.com>
2025-09-30 23:52:22 +0200

    add method to fetch all events from a user

--- src/app/api/uebung-api.service.ts
+++ src/app/api/uebung-api.service.ts
@@ -1,6 +1,7 @@
 import {HttpClient} from '@angular/common/http';
 import {UebungEvent} from '../train-component/domain/uebung';
 import {inject, Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,10 @@ export class UebungApiService {
   httpClient : HttpClient = inject(HttpClient);
 
   BASE_URL = "http://localhost:11000"
+
+  getAllEvents(username : string) : Observable<UebungEvent[]> {
+    return this.httpClient.get<UebungEvent[]>(this.BASE_URL + "/find-events-by-username/" + username)
+  }
 
   postUebungEvent(event : UebungEvent) : void {
     this.httpClient.post<any>(this.BASE_URL+ "/uebung-event", event).subscribe({})
