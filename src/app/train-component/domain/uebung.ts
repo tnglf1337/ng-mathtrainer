4cab47ec4f6c2126360be5321939d96e80c0b13c
Timo Neske <neske.timo93@gmail.com>
2025-09-30 17:16:02 +0200

    implement method createUebungEvent()

--- src/app/train-component/domain/uebung.ts
+++ src/app/train-component/domain/uebung.ts
@@ -1,12 +1,34 @@
 import {ModusTyp} from './modus';
 import {Schwierigkeit} from './schwierigkeit';
+import { v4 as uuidv4 } from 'uuid';
+
 
 export interface UebungEvent {
-  eventid : string;
-  beendetAm : Date;
+  eventId : string;
+  beendetAm : string;
   username : string;
   modus : ModusTyp;
   schwierigkeit : Schwierigkeit;
   korrektLoesungen : number;
   falscheLoesungen : number;
+}
+
+export function createUebungEvent(
+  username : string,
+  modus : ModusTyp,
+  schwierigkeit : Schwierigkeit,
+  korrektLoesungen : number,
+  falscheLoesungen : number) : UebungEvent {
+  const eventId = uuidv4();
+  const beendetAm = new Date().toISOString();
+  return {
+    eventId: eventId,
+    beendetAm: beendetAm,
+    username: username,
+    modus: modus,
+    schwierigkeit: schwierigkeit,
+    korrektLoesungen: korrektLoesungen,
+    falscheLoesungen: falscheLoesungen
+  }
+
 }
