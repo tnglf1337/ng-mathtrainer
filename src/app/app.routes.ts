5982cf39b284f0f96950756c84ddd7f5fe8d7383
Timo Neske <neske.timo93@gmail.com>
2025-09-30 23:32:16 +0200

    add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {TrainComponent} from './train-component/train-component';
+import {HistoryComponent} from './history-component/history-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'uebung',
+    component: TrainComponent
+  },
+  {
+    path: 'history',
+    component: HistoryComponent
+  }
+];
